627177525a68edc6414fb3c7222a358bdf1bb131
zukaChachava <zurabi.chachava.1@btu.edu.ge>
2022-06-22 11:54:38 +0400

    bug fix

--- app/src/main/java/com/lashaandzura/mines/models/Square.kt
+++ app/src/main/java/com/lashaandzura/mines/models/Square.kt
@@ -3,6 +3,5 @@ package com.lashaandzura.mines.models
 data class Square(
     val xCord: Int,
     val yCord: Int,
-    val isMined: Boolean,
-    val nearbyBombsQuantity: Int
+    val isMined: Boolean
 )
