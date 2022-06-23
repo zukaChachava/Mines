cbc9225c763a191c5510b85b27c4e7e25e05a16f
zukaChachava <zurabi.chachava.1@btu.edu.ge>
2022-06-23 18:40:06 +0400

    working on view pager

--- app/src/main/java/com/lashaandzura/mines/MainActivity.kt
+++ app/src/main/java/com/lashaandzura/mines/MainActivity.kt
@@ -2,27 +2,42 @@ package com.lashaandzura.mines
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TableLayout
 import androidx.navigation.NavController
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
+import androidx.viewpager2.widget.ViewPager2
 import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.google.android.material.tabs.TabLayout
+import com.google.android.material.tabs.TabLayoutMediator
+import com.lashaandzura.mines.adapters.ViewPagerAdapter
 import com.lashaandzura.mines.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
     private lateinit var bottomNavigationView: BottomNavigationView
     private lateinit var navController: NavController
+    private lateinit var viewPager2: ViewPager2
+    private lateinit var tableLayout: TabLayout
+    val tabTitle = arrayOf("Zura", "Lasha")
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         binding = ActivityMainBinding.inflate(layoutInflater) // Auto binding, FindViewById is no more needed
         setContentView(binding.root)
-
+        viewPager2 = binding.pager
+        tableLayout = binding.tableLayout
         bottomNavigationView = binding.bottomNavView
         navController = findNavController(R.id.navHostFragment)
+        viewPager2.adapter = ViewPagerAdapter(supportFragmentManager, lifecycle)
+
+        TabLayoutMediator(tableLayout, viewPager2){
+            tab, position ->
+                tab.text = tabTitle[position]
+        }.attach()
 
         setupActionBarWithNavController(navController, AppBarConfiguration(
             setOf(
---
+++ app/src/main/java/com/lashaandzura/mines/adapters/ViewPagerAdapter.kt
@@ -0,0 +1,22 @@
+package com.lashaandzura.mines.adapters
+
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import androidx.lifecycle.Lifecycle
+import androidx.viewpager2.adapter.FragmentStateAdapter
+import com.lashaandzura.mines.fragments.ImageFragment
+
+class ViewPagerAdapter(fragmentManager: FragmentManager, lifecycle: Lifecycle)
+    : FragmentStateAdapter(fragmentManager, lifecycle) {
+
+    override fun getItemCount(): Int {
+        return 2
+    }
+
+    override fun createFragment(position: Int): Fragment {
+        when(position){
+            0 -> return ImageFragment()
+            else -> return ImageFragment()
+        }
+    }
+}
